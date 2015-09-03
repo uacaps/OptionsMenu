7c5d55406b179cd5d5a66bdfd0eb167da3a0f054
Niklas Fahl <nfahl@cs.ua.edu>
2015-09-03 13:47:31 -0500

    - carthage support

---
+++ Framework/OptionsMenu/OptionsMenu/OptionsMenu.h
@@ -0,0 +1,19 @@
+//
+//  OptionsMenu.h
+//  OptionsMenu
+//
+//  Created by Niklas Fahl on 9/3/15.
+//  Copyright © 2015 CAPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OptionsMenu.
+FOUNDATION_EXPORT double OptionsMenuVersionNumber;
+
+//! Project version string for OptionsMenu.
+FOUNDATION_EXPORT const unsigned char OptionsMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OptionsMenu/PublicHeader.h>
+
+
